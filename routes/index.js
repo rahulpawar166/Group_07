6a21199f286e40e5c1abcb37df9ae0a69f93e14f
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-04-28 11:09:01 -0400

    Fixed Private Route For Post

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const usersRoute = require("./users");
 const postRoute = require("./posts")
 const constructorMethod = (app) => {
   app.use("/", usersRoute);
-  app.use("/post", postRoute)
+  app.use("/private/post", postRoute)
   app.use("*", (request, response) => {
     response.status(404).json({
       error: "Not found",
