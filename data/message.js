95c7f1d2eb2f8019b393ee92269db3913c48a1af
anishsubedi5566 <anishsubedi5566@gmail.com>
2022-05-08 13:33:46 -0400

    Message part completed

--- data/message.js
+++ data/message.js
@@ -32,15 +32,57 @@ let exportedMethods = {
   },
 
   async getMessage(user) {
+<<<<<<< Updated upstream
+=======
+    console.log(">>>here user",user)
+    
+    if(!user) throw "Type a user"
+    if(typeof user !== "string") throw "user must be string"
+
+>>>>>>> Stashed changes
     try {
       const messageCollection = await messages();
       const allMessage = await messageCollection.find({ $or: [
         { "sendBy": user },
         { "receivedBy": user}
       ]}).toArray();
+<<<<<<< Updated upstream
       allMessage.map((item) => (item._id = item._id.toString()));
       allMessage.sort((a, b) => b.postDate - a.postDate);
       return allMessage;
+=======
+
+    
+      allMessage.map((item) => (item._id = item._id.toString()));
+      allMessage.sort((a, b) => b.postDate - a.postDate);
+
+     let  x = allMessage.map(item => item.sendBy)
+    .filter((value, index, self) => self.indexOf(value) === index)
+    let y = allMessage.map(item => item.receivedBy)
+    .filter((value, index, self) => self.indexOf(value) === index)
+     let z = x.concat(y)
+     let uniqueuser = [...new Set(z)];
+
+     uniqueuser = uniqueuser.filter(e => e !== user);
+    
+     let uniqmessage = []
+     let constainsMessgaeof = []
+      allMessage = allMessage.reverse()
+    for(let i = 0; i < uniqueuser.length; i++){
+      if(!constainsMessgaeof.includes(uniqueuser[i])) {
+        allMessage.map(each => {
+            if(each.sendBy == uniqueuser[i] || each.receivedBy == uniqueuser[i] ){
+              if(!constainsMessgaeof.includes(uniqueuser[i])){
+              uniqmessage.push(each)
+              constainsMessgaeof.push(uniqueuser[i])
+              }
+            }
+        })
+      }
+     }
+
+      return uniqmessage;
+>>>>>>> Stashed changes
     } catch (error) {
       console.log("error", error);
     }
--- routes/message.js
+++ routes/message.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   
   const allMessage = await postMessage.getMessage(req.session.user.username);
+<<<<<<< Updated upstream
   
   res.render("message/message", {allMessage: allMessage,
    sender: allMessage[0].sendBy,
@@ -22,9 +23,115 @@ router.get("/viewall/:id", async (req, res) => {
 res.render("message/individualMessage", {allMessage: allMessage,
   sender: allMessage[0].sendBy,
   receiver: allMessage[0].receivedBy});
+=======
+  console.log("req.session.user.username",req.session.user.username,allMessage)
+
+  if (allMessage == [] || allMessage == "") {
+    res
+      .status(200)
+      .render("message/message", { noMessage: "There is no message" });
+      return
+  }
+
+  res.status(200).render("message/message", { allMessage: allMessage });
 });
 
+router.get("/groupmessage", async (req, res) => {
+  res.status(200).render("message/groupmessage");
+});
+
+router.post("/groupmessage", async (req, res) => {
+  let allUserArray = xss(req.body.sendto).split(",");
+  let output;
+  let sendBy = req.session.user.username;
+  let date = new Date().toDateString();
+  let message = xss(req.body.message);
+  let i = 0;
+  let receivedBy;
+
+  // try {
+  //   for (let i = 0; i < allUserArray.length; i++) {
+  //     await userData.getUserByUsername(allUserArray[i]);
+  //   }
+  // } catch (e) {
+  //   console.log("error", e);
+  //   res.status(400).render("message/groupmessage", {
+  //     error: "Please verify if all the usernames are correct",
+  //   });
+  //   return;
+  // }
+
+  try {
+    if (xss(req.body.sendto.length < 1)) throw "Send field cannot be empty";
+    if (xss(req.body.message < 1)) throw "Send message cannot be empty";
+  
+    allUserArray = allUserArray.map((each) => each.trim(each));
+    allUserArray = allUserArray.filter((element) => {
+      return element !== "";
+    });
+    for (let i = 0; i < allUserArray.length; i++) {
+      await userData.getUserByUsername(allUserArray[i]);
+    }
+
+
+    for (i = 0; i < allUserArray.length; i++) {
+      receivedBy = allUserArray[i];
+      output = await postMessage.createMessage(
+        message,
+        receivedBy,
+        sendBy,
+        date
+      );
+    }
+    if (output) {
+      res.redirect("/private/message");
+    }
+  } catch (e) {
+    if (e) {
+      console.log("error", e);
+      res.status(400).render("message/groupmessage", { error: e });
+      return;
+    } else {
+      res.status(500).json({
+        error: "Internal Server Error",
+      });
+    }
+  }
+});
+
+router.get("/viewall/:id", async (req, res) => {
+  try {
+    if (!req.params.id) throw "Id cannot be empty";
+    if (typeof req.params.id !== "string") throw "Id cannot be empty";
+    const allMessage = await postMessage.getSpecificMessage(req.params.id);
+    console.log("allMessage", allMessage);
+    if (allMessage.length < 1) {
+      res.render("message/individualMessage", {
+        message: "No conversation is found",
+      });
+    }
+   
+    res.render("message/individualMessage", {
+      allMessage: allMessage,
+      sender: allMessage[0].sendBy,
+      receiver: allMessage[0].receivedBy,
+    });
+  } catch (e) {
+    if (e) {
+      const out = { errors: e };
+      console.log("error", e);
+      res.status(400).render("message/groupmessage", { error: e.message });
+      return;
+    } else {
+      res.status(500).json({
+        error: "Internal Server Error",
+      });
+    }
+  }
+>>>>>>> Stashed changes
+});
 router.post("/", async (req, res) => {
+<<<<<<< Updated upstream
 
     let {message, messageTo,sender,receiver} = req.body
     let sendBy, receivedBy
@@ -35,6 +142,47 @@ router.post("/", async (req, res) => {
     if(req.body.sender || req.body.receiver){
       sendBy =sender;
       receivedBy = receiver
+=======
+  try {
+    let message=xss(req.body.message);
+    let receiver=xss(req.body.receiver)
+    let sendBy;
+    sendBy = req.session.user.username;
+    date = new Date().toDateString();
+    const output = await postMessage.createMessage(
+      message,
+      receiver,
+      sendBy,
+      date
+    );
+
+    if (output) {
+      res.redirect(`/private/message/viewall/${sendBy}-${receiver}`);
+    }
+  } catch (e) {
+    if (e) {
+      const out = { errors: e };
+      res.status(400).render("post/postRoom", { error: e.message });
+      return;
+    } else {
+      res.status(500).json({
+        error: "Internal Server Error",
+      });
+    }
+  }
+});
+
+router.post("/reply", async (req, res) => {
+  try {
+    let message=xss(req.body.message);
+    let sender=xss(req.body.sender);
+    let receiver=xss(req.body.receiver)
+    let sendBy, receivedBy;
+
+    if (xss(req.body.sender) || xss(req.body.receiver)) {
+      sendBy = sender;
+      receivedBy = receiver;
+>>>>>>> Stashed changes
     }
 
     sendBy = req.session.user.username;
@@ -46,19 +194,29 @@ router.post("/", async (req, res) => {
         receivedBy = sender
       }
     }
+<<<<<<< Updated upstream
     
     date = new Date()
    
   try {
+=======
+   
+    date = new Date().toDateString();
+>>>>>>> Stashed changes
     const output = await postMessage.createMessage(
         message,
         receivedBy,
         sendBy,
         date
     );
+<<<<<<< Updated upstream
     
+=======
+
+      
+>>>>>>> Stashed changes
     if (output) {
-      res.redirect("/private");
+      res.redirect(`/private/message/viewall/${sendBy}-${receivedBy}`);
     }
   } catch (e) {
     if (e) {
@@ -73,4 +231,58 @@ router.post("/", async (req, res) => {
   }
 });
 
+<<<<<<< Updated upstream
 module.exports = router;
+=======
+// router.post("/reply", async (req, res) => {
+//   try {
+//     let message=xss(req.body.message);
+//     // let  messageTo=xss(req.body.messageTo);
+//     let sender=xss(req.body.sender);
+//     let receiver=xss(req.body.receiver)
+//     console.log("message, messageTo,sender,receiver",message,sender,receiver)
+//     let sendBy, receivedBy;
+//     // if (xss(req.body.messageTo)) {
+//     //   sendBy = req.session.user.username;
+//     //   receivedBy = receiver;
+//     // }
+//     if (xss(req.body.sender) || xss(req.body.receiver)) {
+//       sendBy = sender;
+//       receivedBy = receiver;
+//     }
+//     sendBy = req.session.user.username;
+//     if (sendBy) {
+//       if (sendBy == sender) {
+//         receivedBy = receiver;
+//       } else {
+//         receivedBy = sender;
+//       }
+//     }
+   
+//     date = new Date().toDateString();
+//     const output = await postMessage.createMessage(
+//       message,
+//       receivedBy,
+//       sendBy,
+//       date
+//     );
+
+      
+//     if (output) {
+//       res.redirect(`/private/message/viewall/${sendBy}-${receivedBy}`);
+//     }
+//   } catch (e) {
+//     if (e) {
+//       const out = { errors: e };
+//       res.status(400).render("post/postRoom", { error: e.message });
+//       return;
+//     } else {
+//       res.status(500).json({
+//         error: "Internal Server Error",
+//       });
+//     }
+//   }
+// });
+
+module.exports = router;
+>>>>>>> Stashed changes
